0d5bc5df7c79066dc4e344b1ca3f129bc3c18654
atyhamos <atyhalive@gmail.com>
2022-07-08 14:34:44 +0800

    Add media queries for increased responsiveness

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let openModalEl = document.getElementById('open-modal')
-let overlayEl = document.getElementById('overlay')
+const openModalEl = document.getElementById('open-modal')
+const overlayEl = document.getElementById('overlay')
 openModalEl.addEventListener('click', () => {
   overlayEl.style.display = 'block'
-})
-
-let closeModalEl = document.getElementById('close-modal')
-closeModalEl.addEventListener('click', () => {
-  overlayEl.style.display = 'none'
+  document.getElementById('close-modal').addEventListener('click', () => {
+    overlayEl.style.display = 'none'
+  })
 })
